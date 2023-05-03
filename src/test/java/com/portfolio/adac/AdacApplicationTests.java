6d377d635f3f374ace36f307a2a169a32dde97c5
AliciaAChavez <132420216+AliciaAChavez@users.noreply.github.com>
2023-05-03 00:31:23 -0300

    Spring initializr
    Creacion del Proyecto BackEnd.

---
+++ src/test/java/com/portfolio/adac/AdacApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.adac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdacApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
